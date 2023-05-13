53f455804c4b179fdcbbd42cddd589b5ce424c20
Lalit572 <anujchaudhari89@gmail.com>
2023-05-13 10:18:27 +0530

    Updated version of site added mongo database

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+// const { default: mongoose } = require("mongoose");
+
+mongoose.connect('mongodb+srv://anujchaudhari89:Anujpass@cluster0.jcj1g0c.mongodb.net/blogDB',{useNewUrlParser: true});
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +22,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts=[];
+
+const postScheme = new mongoose.Schema({
+
+  title : String,
+  content : String
+});
+
+const Post = mongoose.model("Post",postScheme);
+
 
 app.get("/",(req,res)=>{
-  res.render("home",{startingCont:homeStartingContent,posts:posts});
+  
+
+  Post.find({})
+  .exec()
+  .then((posts)=>{
+    res.render("home",{startingCont:homeStartingContent,posts:posts});
+  })
+  .catch((e)=>{
+    console.log(e);
+  })
  
 });
 
@@ -35,30 +58,54 @@ app.get("/compose",(req,res)=>{
   res.render("compose");
 });
 
-app.get("/posts/:data",(req,res)=>{
-    const data = _.lowerCase(req.params.data);
+app.get("/posts/:postid",(req,res)=>{
+    const reqpostid = req.params.postid;
 
-    posts.forEach((ele)=>{
-      const stored = _.lowerCase(ele.title);
-      
-      if(data === stored)
-      {
-        res.render("post",{title:ele.title,content:ele.content});
-      }
-    
-    });
+  Post.findOne({_id: reqpostid})
+  .exec()
+  .then((post)=>{
+    res.render("post",{title:post.title,content:post.content});
+  })
+  .catch((e)=>{
+    console.log(e);
+  });
+
 });
 
 app.post("/compose",(req,res)=>{
   
 
-  const post = {
-    title: req.body.box,
-    content: req.body.postdata
-  };
-  posts.push(post);
- 
+ const post = new Post ({
+  title: req.body.box,
+  content: req.body.postdata
+ });
+
+ post.save()
+ .then(()=>{
   res.redirect("/");
+ })
+ .catch((e)=>{
+      console.log(e);
+ })
+ 
+});
+
+app.post("/delete/:delid", (req, res) => {
+  const deleteid = req.params.delid;
+
+  Post.deleteOne({_id: deleteid})
+  .then(()=>{
+    res.redirect("/");
+  })
+  .catch((e)=>{
+    console.log(e);
+  });
+  // posts = posts.filter((ele) => {
+  //   const stored = _.lowerCase(ele.title);
+  //   return data !== stored;
+  // });
+
+ 
 });
 
 
